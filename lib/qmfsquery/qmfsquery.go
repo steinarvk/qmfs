79cb36afeecba09b9e250976ebc429f1fa25477d
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2019-09-26 21:45:27 +0100

    syntactic structure for query functions
    very very simple, but should do the job

--- lib/qmfsquery/qmfsquery.go
+++ lib/qmfsquery/qmfsquery.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/url"
 	"regexp"
+	"strconv"
 	"strings"
 
 	pb "github.com/steinarvk/qmfs/gen/qmfspb"
@@ -37,6 +38,69 @@ func EntityIDEquals(entityID string) *pb.EntitiesQuery_Clause {
 	}
 }
 
+func parseArgs(unparsedArgs []string, spec string) ([]interface{}, error) {
+	if len(spec) != len(unparsedArgs) {
+		return nil, fmt.Errorf("want %d args got %d (%v)", len(spec), len(unparsedArgs), unparsedArgs)
+	}
+
+	var rv []interface{}
+
+	for i, code := range spec {
+		v := unparsedArgs[i]
+
+		switch code {
+		case 'f':
+			if !ValidPath(v) {
+				return nil, fmt.Errorf("bad filename: %q", v)
+			}
+			fallthrough
+
+		case 's':
+			rv = append(rv, string(v))
+
+		case 'i':
+			n, err := strconv.Atoi(v)
+			if err != nil {
+				return nil, fmt.Errorf("bad number %q: %v", v, err)
+			}
+			rv = append(rv, int(n))
+
+		default:
+			return nil, fmt.Errorf("internal error: unknown arg code %q", code)
+		}
+	}
+
+	return rv, nil
+}
+
+func parseFunctionClause(clausestring string) (*pb.EntitiesQuery_Clause, error) {
+	simp, err := parseSimpleFunction(clausestring)
+	if err != nil {
+		return nil, err
+	}
+
+	clause := &pb.EntitiesQuery_Clause{}
+
+	switch simp.functionName {
+	case "blank":
+		args, err := parseArgs(simp.args, "f")
+		if err != nil {
+			return nil, err
+		}
+
+		clause.Kind = &pb.EntitiesQuery_Clause_FileContents{
+			FileContents: &pb.EntitiesQuery_Clause_FileHasTrimmedContents{
+				Filename: args[0].(string),
+			},
+		}
+
+	default:
+		return nil, fmt.Errorf("unknown query function %q", simp.functionName)
+	}
+
+	return clause, nil
+}
+
 func parseClause(clausestring string) (*pb.EntitiesQuery_Clause, error) {
 	unescaped, err := url.PathUnescape(clausestring)
 	if err != nil {
@@ -48,6 +112,10 @@ func parseClause(clausestring string) (*pb.EntitiesQuery_Clause, error) {
 	if strings.HasPrefix(clausestring, "-") {
 		clausestring = clausestring[1:]
 		clause.Invert = true
+	}
+
+	if strings.Contains(clausestring, "[") {
+		return parseFunctionClause(clausestring)
 	}
 
 	sep := "="
@@ -84,8 +152,87 @@ func parseClause(clausestring string) (*pb.EntitiesQuery_Clause, error) {
 	return clause, nil
 }
 
+type simpleFunction struct {
+	functionName string
+	args         []string
+}
+
+func parseSimpleFunction(s string) (*simpleFunction, error) {
+	singleComp, err := splitQuerystring(s)
+	if err != nil {
+		return nil, err
+	}
+	if len(singleComp) != 1 {
+		return nil, fmt.Errorf("malformed function call clause (top-level comma): %q", s)
+	}
+
+	if !strings.Contains(s, "[") || !strings.Contains(s, "]") {
+		return nil, fmt.Errorf("malformed function call clause (must contain brackets): %q", s)
+	}
+
+	argsStart := strings.Index(s, "[")
+	argsEnd := strings.LastIndex(s, "]")
+
+	unparsedArgsString := strings.TrimSpace(s[argsStart+1 : argsEnd])
+
+	unparsedArgs, err := splitQuerystring(unparsedArgsString)
+	if err != nil {
+		return nil, fmt.Errorf("malformed function call clause (bad args): %v", err)
+	}
+
+	functionName := s[:argsStart]
+	if strings.TrimSpace(functionName) == "" {
+		return nil, fmt.Errorf("malformed function call clause (no function name): %q", s)
+	}
+
+	return &simpleFunction{functionName, unparsedArgs}, nil
+}
+
+func splitQuerystring(s string) ([]string, error) {
+	sep := ','
+	inc := '['
+	dec := ']'
+
+	var rv []string
+	var level int
+	var collected []rune
+
+	flush := func() {
+		if len(collected) > 0 {
+			rv = append(rv, string(collected))
+		}
+		collected = nil
+	}
+
+	for _, ch := range s {
+		switch ch {
+		case sep:
+			if level == 0 {
+				flush()
+				continue
+			}
+		case inc:
+			level++
+		case dec:
+			if level == 0 {
+				return nil, fmt.Errorf("unmatched %q", dec)
+			}
+			level--
+		}
+		collected = append(collected, ch)
+	}
+
+	flush()
+
+	return rv, nil
+}
+
 func Parse(querystring string) (*pb.EntitiesQuery, error) {
-	clausestrings := strings.Split(querystring, ",")
+	clausestrings, err := splitQuerystring(querystring)
+	if err != nil {
+		return nil, err
+	}
+
 	query := &pb.EntitiesQuery{}
 	for _, clausestring := range clausestrings {
 		clauseq, err := parseClause(clausestring)
